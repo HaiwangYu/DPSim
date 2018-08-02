53690da61bee40005f97ea30d98ba30e8b9f6181
HaiwangYu <yuhw.pku@gmail.com>
2018-08-02 09:36:57 -0500

    update setup for me

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 export DPSIM_ROOT=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 export PATH=$PATH:$DPSIM_ROOT/build/bin
+export PATH=/e906/app/users/liuk/sw/geant/geant4.10.01.p03-install/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DPSIM_ROOT/build/lib
